67bccbfa04c7a986e013827fad1dc56d5210ec9d
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-15 19:59:53 +0800

    字体图标--自己解决了bug真好

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+const pxToViewport = require('postcss-px-to-viewport')
+const vw = pxToViewport({
+  // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+  viewportWidth: 375
+})
 
 module.exports = {
   // webpack 配置
@@ -10,6 +15,17 @@ module.exports = {
       // 约定：使用@sass表示全局SASS样式所在路径
       // 在sass中使用
       '@scss': path.resolve(__dirname, 'src/styles')
+    }
+  },
+  style: {
+    postcss: {
+      mode: 'extends',
+      loaderOptions: {
+        postcssOptions: {
+          ident: 'postcss',
+          plugins: [vw]
+        }
+      }
     }
   }
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,3 +1,5 @@
+import './index.scss'
+
 const Login = () => {
   return (
     <div>
