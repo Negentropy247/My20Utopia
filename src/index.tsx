dc514f2b0894b5452df50706f6b66f59215851d3
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-17 10:39:42 +0800

    统一格式化代码--哭哭捏

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '@/index.scss'
-import App from './App'
-import { HashRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/index.scss";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from "react-redux";
+import store from "./store";
 
-import { HistoryRouter, history } from './router/history'
+import { HistoryRouter, history } from "./router/history";
 
 // 创建应用根实例
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
     <HistoryRouter history={history}>
       <App />
     </HistoryRouter>
-  </Provider>
-)
+  </Provider>,
+);
